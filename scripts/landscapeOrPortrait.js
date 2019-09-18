6c857f1ec210ec4ae6ea70aa2ae72bffdfeb81a0
Dawson Murray <d.murray3@EDU.epsb.ca>
2019-09-18 13:56:04 -0600

    m

--- scripts/landscapeOrPortrait.js
+++ scripts/landscapeOrPortrait.js
@@ -1,1 +1,21 @@
 console.log("you made it to landscapeOrPortrait.js")
+
+var VarWidth = 500;
+var VarHeight = 500;
+function isLandscape (width, height) {
+  if (width == height) return 'Square';
+  return (width > height) ? 'Landscape' : 'Portrait';
+}
+
+function inputGeometry(VarWidth, VarHeight) {
+  var VarWidth = prompt("Please enter an interger value for width", "500");
+  var VarHeight = prompt("Please enter an interger value for height", "500");
+  if (!(isNaN(VarWidth) || isNaN(VarHeight))){
+    let width = VarWidth;
+    let height = VarHeight;
+    console.log("This display geometry is", isLandscape (width, height));
+   }
+  else {
+    inputGeometry(VarWidth, VarHeight)
+  }
+}
